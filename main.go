581c2caa245ba49da81ebd30ee88d5da7dde38e2
ar3ty <ar3tyrt5y@gmail.com>
2025-08-17 22:20:56 +0200

    reset, metrics, healthz routes

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
+	cuurentDir := "."
 	port := "8080"
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	handlerFile := http.StripPrefix("/app", http.FileServer(http.Dir(cuurentDir)))
 
 	mux := http.NewServeMux()
-
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handlerFile))
+	mux.HandleFunc("GET /healthz", handleReadiness)
+	mux.HandleFunc("GET /metrics", apiCfg.handleMetrics)
+	mux.HandleFunc("POST /reset", apiCfg.resetMetrics)
 
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
 	}
-	server.ListenAndServe()
+
+	log.Printf("Serving on port: %s", port)
+	log.Fatal(server.ListenAndServe())
 }
---
+++ metrics.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+)
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	text := "Hits: " + strconv.Itoa(int(cfg.fileserverHits.Load()))
+	w.Write([]byte(text))
+}
